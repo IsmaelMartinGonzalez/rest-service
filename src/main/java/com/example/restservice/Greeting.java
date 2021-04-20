9c8e162b78b7d4923484a26b9c7852332e63cde7
ismael <fmartin@gmail.com>
2021-04-20 16:44:24 +0200

    Initial commit

---
+++ src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,31 @@
+package com.example.restservice;
+
+/**
+ * Project name: DAM20/PACKAGE_NAME
+ * Filename:
+ * Created:  14/11/2020 / 13:47
+ * Description:
+ * Revision:
+ *
+ * @Author: Ismael - fmartin@nigul.cide.es
+ * @Version:
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
